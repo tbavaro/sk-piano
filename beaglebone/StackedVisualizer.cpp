e9debe63d3ae0b761a274ec94ba6094e5c26a9bc
David <daveey@asana.com>
2012-08-23 02:21:47 -0700

    adds stacked visualizer

---
+++ beaglebone/StackedVisualizer.cpp
@@ -0,0 +1,46 @@
+#include "StackedVisualizer.h"
+#include "Colors.h"
+#include "Util.h"
+#include <string.h>
+
+StackedVisualizer::StackedVisualizer(LightStrip& strip) : LightStripVisualizer(strip) {
+  pixels = new Color[strip.numPixels()];
+  age = strip.numPixels();
+}
+
+StackedVisualizer::~StackedVisualizer() {
+  delete[] pixels;
+}
+
+void StackedVisualizer::onKeyDown(Key key) {
+  Color c = Colors::hsv(360/88*key, 1.0, 1.0);
+  pixels[0] = Colors::add(pixels[0], c); 
+}
+
+void StackedVisualizer::onKeyUp(Key key) {
+}
+  
+void StackedVisualizer::onPassFinished(bool something_changed) {
+  for (int fps = 0; fps < 10; ++fps) {
+    if (Colors::BLACK != pixels[strip.numPixels() - 1]) {
+      age--;
+    }
+    if (age == 0) {
+      pixels[strip.numPixels() - 1] = Colors::BLACK;
+      age = strip.numPixels();
+    }
+    for (int i = strip.numPixels() - 1; i > 0; --i) {
+      if (Colors::BLACK == pixels[i]) {
+        pixels[i] = pixels[i-1];
+        pixels[i-1] = Colors::BLACK;
+  	  }
+      strip.setPixel(i, pixels[i]);
+    }    
+  }
+}
+
+void StackedVisualizer::reset() {
+  LightStripVisualizer::reset();
+  bzero(pixels, strip.numPixels() * sizeof(Color));
+}
+
