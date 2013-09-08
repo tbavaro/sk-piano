ae879f5a4c1a9dc73577e0c8db9b859e15815921
tim <tim@asana.com>
2013-09-08 14:32:58 -0700

    magic number cleanup

--- node-spi/src/spi.cpp
+++ node-spi/src/spi.cpp
@@ -25,13 +25,19 @@ static const bool IS_STUB = false;
 
 static const int BITS_PER_WORD = 8;
 
+// NB: in the old environment this was 0, but now it seems to always be 1
+static const int EXPECTED_READ_MODE = 1;
+
+static const int FD_NONE = -1;
+static const int IOCTL_FAIL = -1;
+
 Spi::Spi(const char* spiDevice, uint32_t _maxSpeedHz)
-    : fd(-1), maxSpeedHz(_maxSpeedHz) {
+    : fd(FD_NONE), maxSpeedHz(_maxSpeedHz) {
   if (IS_STUB) {
     return;
   }
 
-  fd = open(spiDevice, O_RDWR, 0);
+  fd = open(spiDevice, O_RDWR);
   if (fd < 0) {
     throw RuntimeException("unable to open SPI device: %s", spiDevice);
   }
@@ -40,23 +46,25 @@ Spi::Spi(const char* spiDevice, uint32_t _maxSpeedHz)
     uint8_t tmp8;
     uint32_t tmp32;
 
-    if (ioctl(fd, SPI_IOC_RD_MODE, &tmp8) == -1) {
+    if (ioctl(fd, SPI_IOC_RD_MODE, &tmp8) == IOCTL_FAIL) {
       throw RuntimeException("unable to get read mode");
-    } /* else if (tmp8 != 0) {
-      throw RuntimeException("only mode 0 supported; got: %d", tmp8);
-    } */
+    } else if (tmp8 != EXPECTED_READ_MODE) {
+      throw RuntimeException("expected read mode %d, got: %d",
+          EXPECTED_READ_MODE, tmp8);
+    }
 
-    if (ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &tmp8) == -1) {
+    if (ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &tmp8) == IOCTL_FAIL) {
       throw RuntimeException("unable to get bits per word");
     } else if (tmp8 != BITS_PER_WORD) {
-      throw RuntimeException(
-            "only %d bits per word supported; got: %d",
+      throw RuntimeException("expected %d bits per word, got: %d",
             BITS_PER_WORD, tmp8);
     }
 
-    ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &maxSpeedHz);
+    if (ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &maxSpeedHz) == IOCTL_FAIL) {
+      throw RuntimeException("unable to set max speed to %d", maxSpeedHz);
+    }
 
-    if (ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &tmp32) == -1) {
+    if (ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &tmp32) == IOCTL_FAIL) {
       throw RuntimeException("unable to get max speed");
     } else if (tmp32 != maxSpeedHz) {
       throw RuntimeException(
@@ -74,9 +82,9 @@ Spi::~Spi() {
 }
 
 void Spi::close() {
-  if (fd >= 0) {
+  if (fd != FD_NONE) {
     ::close(fd);
-    fd = -1;
+    fd = FD_NONE;
   }
 }
 
@@ -97,6 +105,10 @@ void Spi::send(void* buf, int numBytes) {
     }
     printf("\n");
     return;
+  }
+
+  if (fd == FD_NONE) {
+    throw RuntimeException("SPI instance has been closed");
   }
 
   int ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
