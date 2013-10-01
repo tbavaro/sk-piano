c0b8a8309ed81a925676ba4bb3fa217dc9a2813c
Linux User <piano@piano.(none)>
2013-10-01 22:11:43 +0000

    init gpio pins

--- scripts/simple/run.sh
+++ scripts/simple/run.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+/home/piano/git/piano/scripts/simple/export_gpio_pins.sh
+
 cd /home/piano/git/piano/piano
-make run
+while [ true ]
+do
+  make run
+  
+  # sleep so we don't thrash if there's a problem starting
+  sleep 1
+done
 
